65ed48519de1a18959da03cccb2a3596ed2dd7a1
Heiko Stumpf <mail@heikostumpf.de>
2018-08-23 19:57:43 +0200

    Initial commit of aws-dynamodb project

---
+++ src/test/java/de/heikostumpf/aws/dynamodb/DynamodbApplicationTests.java
@@ -0,0 +1,16 @@
+package de.heikostumpf.aws.dynamodb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DynamodbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
